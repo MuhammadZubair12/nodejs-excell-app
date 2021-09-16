c7f9022607cba917aa2f504aa6184835254ee76e
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-16 20:48:53 +0500

    a

--- config/connection.js
+++ config/connection.js
@@ -9,11 +9,20 @@ let pool_config = {
   operatorsAliases: false,
   autoreconnect: true,
 }
+// const development = {
+//   database: 'countifier',
+//   username: 'postgres',
+//   password: 'c00822631',
+//   host: 'localhost',
+//   dialect: 'postgres',
+//   port:5432,
+//   pool: pool_config,
+// };
 const development = {
-  database: 'countifier',
-  username: 'postgres',
-  password: 'c00822631',
-  host: 'localhost',
+  database: 'da1dvo9ivmcgk2',
+  username: 'fyherdyowangiq',
+  password: '9e3b2ca441fc0dcb00bb400b8651f57dab3507a5119ab007507ed1d6c9a2e892',
+  host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
   dialect: 'postgres',
   port:5432,
   pool: pool_config,
@@ -24,7 +33,9 @@ const production = {
   username: 'fyherdyowangiq',
   password: '9e3b2ca441fc0dcb00bb400b8651f57dab3507a5119ab007507ed1d6c9a2e892',
   host: 'ec2-34-227-120-94.compute-1.amazonaws.com',
-  dialect: 'postgres'
+  dialect: 'postgres',
+  port:5432,
+  pool: pool_config,
 };
 
 module.exports = {
