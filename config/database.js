f92e9e7bb0838aaab5ffea093d0913dcc333acf6
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-20 20:23:33 +0500

    a

--- config/database.js
+++ config/database.js
@@ -5,22 +5,21 @@ const connection = require('./connection');
 
 let database;
 
-
 switch (process.env.NODE_ENV) {
   case 'production':
     database = new Sequelize(
-      connection.testing.database,
-      connection.testing.username,
-      connection.testing.password, {
-        host: connection.testing.host,
-        dialect: connection.testing.dialect,
+      connection.production.database,
+      connection.production.username,
+      connection.production.password, {
+        host: connection.production.host,
+        dialect: connection.production.dialect,
         operatorsAliases: false,
-        port:connection.testing.port,
         dialectOptions: {
           ssl: { rejectUnauthorized: false },
           useUTC: true,
           sslmode: require
         },
+        port:connection.production.port,
         pool: {
           max: 5,
           min: 0,
@@ -51,15 +50,37 @@ switch (process.env.NODE_ENV) {
         },
       });
     break;
+    case 'development':
+    database = new Sequelize(
+      connection.development.database,
+      connection.development.username,
+      connection.development.password, {
+        host: connection.development.host,
+        dialect: connection.development.dialect,
+        operatorsAliases: false,
+        port:connection.development.port,
+        dialectOptions: {
+          ssl: { rejectUnauthorized: false },
+          useUTC: true,
+          sslmode: require
+        },
+        pool: {
+          max: 5,
+          min: 0,
+          idle: 10000,
+          acquire: 20000
+        },
+      });
+    break;
   default:
   database = new Sequelize(
-    connection.development.database,
-    connection.development.username,
-    connection.development.password, {
-      host: connection.development.host,
-      dialect: connection.development.dialect,
+    connection.production.database,
+    connection.production.username,
+    connection.production.password, {
+      host: connection.production.host,
+      dialect: connection.production.dialect,
       operatorsAliases: false,
-      port:connection.development.port,
+      port:connection.production.port,
       dialectOptions: {
         ssl: { rejectUnauthorized: false },
         useUTC: true,
