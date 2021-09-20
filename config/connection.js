cc52ddaac37c5e53dca7799c5dca58c9d21e871a
MuhammadZubair12 <zubairmazhar23@gmail.com>
2021-09-20 19:13:56 +0500

    k

--- config/connection.js
+++ config/connection.js
@@ -9,33 +9,31 @@ let pool_config = {
   operatorsAliases: false,
   autoreconnect: true,
 }
-// const development = {
-//   database: 'countifier',
-//   username: 'postgres',
-//   password: 'c00822631',
-//   host: 'localhost',
-//   dialect: 'postgres',
-//   port:5432,
-//   pool: pool_config,
-// };
 const development = {
-  database: 'dal1chp6l0krci',
-  username: 'niwlzaixfyqhxh',
-  password: '02e838c87686125ef060f92d39dc70032941a5d985ad59475b2101ddfcb44bc3',
-  host: 'ec2-18-209-143-227.compute-1.amazonaws.com',
+  database: 'countifier',
+  username: 'postgres',
+  password: 'c00822631',
+  host: 'localhost',
   dialect: 'postgres',
   port:5432,
   pool: pool_config,
 };
+// const development = {
+//   database: 'dal1chp6l0krci',
+//   username: 'niwlzaixfyqhxh',
+//   password: '02e838c87686125ef060f92d39dc70032941a5d985ad59475b2101ddfcb44bc3',
+//   host: 'ec2-18-209-143-227.compute-1.amazonaws.com',
+//   dialect: 'postgres',
+//   port:5432,
+//   pool: pool_config,
+// };
 
 const production = {
   database: 'dal1chp6l0krci',
   username: 'niwlzaixfyqhxh',
   password: '02e838c87686125ef060f92d39dc70032941a5d985ad59475b2101ddfcb44bc3',
   host: 'ec2-18-209-143-227.compute-1.amazonaws.com',
-  dialect: 'postgres',
-  port:5432,
-  pool: pool_config,
+  dialect: 'postgres'
 };
 
 const testing = {
